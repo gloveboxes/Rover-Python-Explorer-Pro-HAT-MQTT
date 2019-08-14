d2aec9a2cd24c19129e2dbacd9765f5c4821951f
Dave Glover <dglover@microsoft.com>
2019-08-14 10:45:22 +1000

    tidy code and launcher

--- rover-mqtt.py
+++ rover-mqtt.py
@@ -2,15 +2,15 @@ import explorerhat
 import paho.mqtt.client as mqtt
 import time
 
-motor1 = explorerhat.motor.one
-motor2 = explorerhat.motor.two
-
-
+leftMotor = explorerhat.motor.one
+rightMotor = explorerhat.motor.two
+roverMotorMqttTopic = "/rover/motor"
 
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code: %s" % rc)
-    client.subscribe("/rover/motor")
+    client.subscribe(roverMotorMqttTopic)
+
 
 def on_disconnect(client, userdata, rc):
     print("Disconnected with result code: %s" % rc)
@@ -19,36 +19,42 @@ def on_disconnect(client, userdata, rc):
 def on_message(client, userdata, msg):
     print("{0} - {1} ".format(msg.topic, str(msg.payload)))
     motor(msg.payload)
-    # Do this only if you want to send a reply message every time you receive one
-    # client.publish("devices/mqtt/messages/events", "REPLY", qos=1)
+
 
 def stop():
-    motor1.stop()
-    motor2.stop()
+    leftMotor.stop()
+    rightMotor.stop()
+
 
 def forward():
-    motor1.forward()
-    motor2.forward()
+    leftMotor.forward()
+    rightMotor.forward()
+
 
 def backwards():
-    motor1.backwards()
-    motor2.backwards()
+    leftMotor.backwards()
+    rightMotor.backwards()
+
 
 def left():
-    motor1.stop()
-    motor2.forward()
+    leftMotor.forward()
+    rightMotor.stop()
 
-def circle_left():
-    motor1.backwards()
-    motor2.forward()
 
 def right():
-    motor1.forward()
-    motor2.stop()
+    leftMotor.stop()
+    rightMotor.forward()
+
+
+def circle_left():
+    leftMotor.backwards()
+    rightMotor.forward()
+
 
 def circle_right():
-    motor1.forward()
-    motor2.backwards()
+    leftMotor.forward()
+    rightMotor.backwards()
+
 
 def motor(direction):
     switcher = {
@@ -58,19 +64,12 @@ def motor(direction):
         b'3': right,
         b'6': backwards,
         b'7': circle_left,
-        b'8': circle_right,
-        8: "August",
-        9: "September",
-        10: "October",
-        11: "November",
-        12: "December"
+        b'8': circle_right
     }
 
     func = (switcher.get(direction))
     if func is not None:
         func()
-    # print(switcher.get(direction, "Invalid month"))
-
 
 
 client = mqtt.Client('rpirover', mqtt.MQTTv311)
@@ -80,14 +79,10 @@ client.on_disconnect = on_disconnect
 client.on_message = on_message
 
 
-client.connect("localhost") # connect to local mosquitto service for testing purposes
+client.connect("localhost")
 
 client.loop_start()
 
 
 while True:
     time.sleep(999999)
-
-
-motor1.forward()
-motor2.forward()
